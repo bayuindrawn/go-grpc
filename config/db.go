3a84055575e88da136f9df7d403585d3d7578f2a
Bayu Indrawan <bayuindrawan95@gmail.com>
2025-07-14 15:41:06 +0700

    update local db connection

--- config/db.go
+++ config/db.go
@@ -8,7 +8,7 @@ import (
 )
 
 func ConnectDB() *gorm.DB {
-	dsn := "root:password@tcp(localhost:3306)/employees?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := "root:secret@tcp(localhost:3306)/employees?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatalf("Failed to connect to DB: %v", err)
